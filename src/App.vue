004460100f4a55e46c9d7570609ecc90a8fd93d0
cbrgpl <cbrgpl@gmail.com>
2023-03-13 15:55:31 +0700

    feat: make alg but it blocks thread;

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts" >
 import { setLanguageAtInit } from './utils/composable/setLanguageAtInit'
+import { localStorageManager } from '@modules/LocalStorageManager'
 
 setLanguageAtInit()
+const searchStorageLimit = async () => {
+  console.log( 'Найдено значение! ', await localStorageManager.searchStorageLimit() )
+}
 </script>
 
 <template >
   <v-app >
+    <v-btn
+      color="success"
+      @click="searchStorageLimit" >
+      text
+    </v-btn>
     <router-view />
   </v-app>
 </template>
---
+++ src/submodules/profile/layouts/LayoutProfile/partials/PStorageWidget.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts" >
+import { ref } from 'vue'
+
+const helpButtonVisible = ref( false )
+
+
+</script>
+
+<template >
+  <v-card
+    class="storage-widget"
+    variant="text" >
+    <v-progress-circular
+      class="storage-widget__circular"
+      color="orange"
+      :model-value="75"
+      :size="50"
+      :width="12"
+      @click="helpButtonVisible = !helpButtonVisible" >
+    </v-progress-circular>
+
+    <v-fade-transition >
+      <v-btn
+        v-if="helpButtonVisible"
+        width="32px"
+        min-width="24px"
+        height="32px"
+        class="storage-widget__help" >
+        <v-icon
+          icon="help-circle"
+          size="32px" />
+      </v-btn>
+    </v-fade-transition>
+  </v-card>
+</template>
+
+<style scoped lang="scss" >
+.storage-widget {
+  position: relative;
+  overflow: visible;
+  cursor: pointer;
+}
+
+.storage-widget__circular {
+}
+
+.storage-widget__help {
+  position: absolute;
+  right: rem(-5px);
+  bottom: rem(10px);
+  border-radius: 100%;
+  transform: translate(50%, 50%);
+}
+
+.fade-transition-enter-active {
+  transition: all 80ms ease;
+}
+
+</style>
